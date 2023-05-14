bd63ad310665b2e9f962b3e7d5d85561ebc935cc
kimsooyoung <tge1375@naver.com>
2023-05-14 21:18:27 +0900

    custom hw done

--- my_robot_hardware_interface/launch/bring_up_on_hardware.launch.py
+++ my_robot_hardware_interface/launch/bring_up_on_hardware.launch.py
@@ -1,35 +1,69 @@
 import os
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch_ros.actions import Node
 import xacro
 
+from launch import LaunchDescription
+from launch.event_handlers import OnProcessExit
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+
+from launch.conditions import IfCondition
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.actions import ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+
+from launch_ros.actions import Node
+
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
-    robot_name = "rrbot"
-    robot_model_file = "rrbot.xacro"
-    package_name = "rrbot_unit3"
+    # robot_name = "rrbot"
+    # robot_model_file = "rrbot.xacro"
+    # package_name = "rrbot_unit3"
 
-    rviz_config = os.path.join(get_package_share_directory(
-        "rrbot_unit3"), "rviz", "unit3.rviz")
 
-    robot_description = os.path.join(get_package_share_directory(
-        package_name), "urdf", robot_model_file)
 
-    robot_description_config = xacro.process_file(robot_description)
+    # robot_description = os.path.join(get_package_share_directory(
+    #     package_name), "urdf", robot_model_file)
+
+    # robot_description_config = xacro.process_file(robot_description)
+
+
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("gz_rrbot_ros2"), "urdf", "rrbot_custom_hw.xacro"]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
 
     controller_config = os.path.join(
         get_package_share_directory(
             "my_robot_hardware_interface"), "config", "controller_configuration.yaml"
     )
 
+    rviz_config = os.path.join(
+        get_package_share_directory("gz_rrbot_ros2"), 
+        "rviz", "custom_hw.rviz"
+    )
+
     return LaunchDescription([
+
+        Node(
+            package="robot_state_publisher",
+            executable="robot_state_publisher",
+            output="screen",
+            parameters=[robot_description],
+        ),
+
         Node(
             package="controller_manager",
             executable="ros2_control_node",
-            parameters=[
-                {"robot_description": robot_description_config.toxml()}, controller_config],
+            parameters=[robot_description, controller_config],
             output={
                 "stdout": "screen",
                 "stderr": "screen",
@@ -38,32 +72,45 @@ def generate_launch_description():
 
         Node(
             package="controller_manager",
-            executable="spawner",
-            arguments=["joint_state_broadcaster",
-                       "--controller-manager", "/controller_manager"],
+            executable="spawner.py",
+            arguments=["joint_state_broadcaster"],
+            output="screen",
         ),
 
         Node(
             package="controller_manager",
-            executable="spawner",
-            arguments=["forward_position_controller",
-                       "-c", "/controller_manager"],
+            executable="spawner.py",
+            arguments=["forward_position_controller"],
+            output="screen",
         ),
 
         Node(
             package="controller_manager",
-            executable="spawner",
-            arguments=["joint_trajectory_controller",
-                       "-c", "/controller_manager"],
+            executable="spawner.py",
+            arguments=["joint_trajectory_controller"],
+            output="screen",
         ),
 
-        Node(
-            package="robot_state_publisher",
-            executable="robot_state_publisher",
-            name="robot_state_publisher",
-            parameters=[
-                {"robot_description": robot_description_config.toxml()}],
-            output="screen"),
+        # Node(
+        #     package="controller_manager",
+        #     executable="spawner.py",
+        #     arguments=["joint_state_broadcaster",
+        #                "--controller-manager", "/controller_manager"],
+        # ),
+
+        # Node(
+        #     package="controller_manager",
+        #     executable="spawner.py",
+        #     arguments=["forward_position_controller",
+        #                "-c", "/controller_manager"],
+        # ),
+
+        # Node(
+        #     package="controller_manager",
+        #     executable="spawner.py",
+        #     arguments=["joint_trajectory_controller",
+        #                "-c", "/controller_manager"],
+        # ),
 
         Node(
             package="rviz2",
